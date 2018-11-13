3ced028248670bb5b2cc1bc6a4d9b2a8ca3b4790
pieworkplace <pieworkplace@gmail.com>
2018-11-13 18:38:57 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,31 +1,33 @@
+from flask import json
 from sqlalchemy import Table, create_engine, MetaData
-from flask import Flask, json
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
+from config import DB_URL
 
-# Here changed the default database name ultrax -> discuz
-DB_URL = "mysql+pymysql://root:@localhost/discuz"
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
-
-db = SQLAlchemy(app)
-
-base = declarative_base()
+Base = declarative_base()
 engine = create_engine(DB_URL)
 metadata = MetaData(bind=engine)
 
+class Member(Base):
+    __table__ = Table('pre_common_member', metadata, autoload=True)
 
-class Member(base):
-    __table__ = Table('pre_ucenter_members', metadata, autoload=True)
+# 需要在discuz数据库中手动设置主键
+class Session(Base):
+    __table__ = Table('pre_common_session', metadata, autoload=True)
+
+class Thread(Base):
+    __table__ = Table('pre_forum_thread', metadata, autoload=True)
 
     def __repr__(self):
-        return json.dumps({"Member Info": {'name': self.username, 'email': self.email}})
+        return json.dumps({"thread info": {'subject ': self.subject, 'author': self.author}})
 
+class Profile(Base):
+    __table__ = Table('pre_common_member_profile', metadata, autoload=True)
 
+    def __repr__(self):
+        return json.dumps({"user profile": {'uid ': self.uid, 'gender': self.gender}})
 
-if __name__ == '__main__':
-    # new_common_member = Member(username='pieworkplace', password='12345678', email='pieworkplace@gmail.com', salt='123')
-    # db.session.add(new_common_member)
-    # db.session.commit()
-    print(db.session.query(Member).all())
+class Message(Base):
+    __table__ = Table('pre_ucenter_pm_lists', metadata, autoload=True)
+
+    def __repr__(self):
+        return json.dumps({"message": {'authorid': self.authorid, 'subject': self.subject}})
